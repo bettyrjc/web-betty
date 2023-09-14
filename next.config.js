52ea4a3f8300fe4e0ab7bb99353fda5584b486be
Betty Jimenez <bettyjimenez3010@gmail.com>
2023-09-14 12:12:38 -0400

    :sparkles: add pull request yaml

--- next.config.js
+++ next.config.js
@@ -2,12 +2,13 @@ const { i18n } = require('./next-i18next.config');
 
 const nextConfig = {
   // i18n,
+  reactStrictMode: true,
   eslint: {
     dirs: ['src'],
   },
   images: {
     loader: 'akamai',
-    path: '',
+    path: './',
   },
   assetPrefix: './',
   // experimental: {
--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+
 import Footer from './layout/Footer';
 import Header from './layout/Header';
 
