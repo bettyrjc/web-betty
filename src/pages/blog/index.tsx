df48e10b2fd364617a272b174b3343c66e06a5c3
Betty Jimenez <bettyjimenez3010@gmail.com>
2024-10-24 18:49:18 -0300

    :ambulance: fix build

--- src/pages/blog/index.tsx
+++ src/pages/blog/index.tsx
@@ -1,17 +1,12 @@
 import React from 'react';
 
-import Card from 'src/modules/shared/large-card';
 import Layout from 'src/modules/layout/Layout';
 
 const BlogView = () => {
   return (
     <Layout>
       <div className="h-full">
-        <div className="grid grid-cols-2 px-48 py-20 gap-14 ">
-          <Card />
-          <Card />
-          <Card />
-        </div>
+        <div className="grid grid-cols-2 px-48 py-20 gap-14 "></div>
       </div>
     </Layout>
   );
