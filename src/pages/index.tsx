ed48e04b88fcdb69b7d181448630b61ae517016c
Betty Jimenez <bettyjimenez3010@gmail.com>
2024-10-24 19:42:27 -0300

    :sparkles: add: plausible

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -18,15 +18,19 @@ const Home: NextPage = () => {
         <link rel="icon" href="/images/icon.png" />
       </Head>
       <Script
-        id="gtm-script"
+        defer
+        data-domain="betjimenez.com"
+        src="https://plausible.io/js/script.hash.outbound-links.pageview-props.tagged-events.js"
+        strategy="afterInteractive"
+      />
+      <Script
+        id="plausible-setup"
         strategy="afterInteractive"
         dangerouslySetInnerHTML={{
           __html: `
-              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-              })(window,document,'script','dataLayer','GTM-5NGDG9RW');
+              window.plausible = window.plausible || function() { 
+                (window.plausible.q = window.plausible.q || []).push(arguments) 
+              }
             `,
         }}
       />
@@ -60,14 +64,6 @@ const Home: NextPage = () => {
           <IconPack />
         </div>
       </div>
-      <noscript>
-        <iframe
-          src="https://www.googletagmanager.com/ns.html?id=GTM-5NGDG9RW"
-          height="0"
-          width="0"
-          style={{ display: 'none', visibility: 'hidden' }}
-        ></iframe>
-      </noscript>
     </Layout>
   );
 };
